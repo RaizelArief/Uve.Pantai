d544194c5ffab0b8e5a8eb35cd816c759c1c662f
RaizelArief <dianuve@gmail.com>
2024-08-31 20:59:17 +0700

    Update

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 function Footer() {
     return (
         <>
-            <div className="Foot" id="footer">
+            <div className="Foot">
                 <div className="Foot1 flex align-top">
+                    <a href="https://wa.me/6282112992879">
                     <img src="https://cdn-icons-png.flaticon.com/128/5968/5968841.png" alt="" className="image" />
+                    </a>
                     <h1>0821-1299-2879</h1>
                 </div>
 
